fd45966ec030df10a166714da88e1077c236e8b1
charliekliger <charliejae16@gmail.com>
2022-12-07 19:45:44 -0500

    Added additional logic to HuffmanTree.

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -9,15 +9,33 @@ public class HuffmanTree
 
     public void printLegend()
     {
-
+        printLegend(root, "");
     }
 
-    public static BinaryHeap legendToHeap(String legend)
+    private void printLegend(HuffmanNode t, String s)
     {
-        return null;
+        if (t.letter.length() > 1)
+        {
+            printLegend(t.left, s + "0");
+            printLegend(t.right, s + "1");
+        }
+
+        if (t.letter.length() == 1)
+            System.out.println(t.letter + "=" + s);
     }
 
-    public static HuffmanTree createFromHeap(BinaryHeap b)
+    public static BinaryHeap<HuffmanNode> legendToHeap(String legend)
+    {
+        String[] legendArr = legend.split(" "); // ["A", "20", "E", "24"]
+        BinaryHeap<HuffmanNode> heap = new BinaryHeap<>();
+
+        for (int i = 0; i <= legendArr.length - 2; i += 2)
+            heap.insert(new HuffmanNode(legendArr[i], Double.parseDouble(legendArr[i + 1])));
+
+        return heap;
+    }
+
+    public static HuffmanTree createFromHeap(BinaryHeap<HuffmanNode> b)
     {
         return null;
     }
